f6c98c6d957f04fa9d27ca6338f367b0a2ea43ba
samarth kashyap <samarthkashyap318@gmail.com>
2023-08-16 13:00:29 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-red-500 w-96 h-96"><h1 className=' text-9xl'>Chandra Farms</h1></div>
+    </>
+  )
+}
+
+export default App
