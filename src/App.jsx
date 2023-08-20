9e5b002d445897bd0ceb477be24cd8dee3490a9c
samarth kashyap <samarthkashyap318@gmail.com>
2023-08-20 23:07:48 +0530

    Navbar changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-red-500 w-96 h-96"><h1 className=' text-9xl'>Chandra Farms</h1></div>
+      <header><Navbar/></header>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,21 @@ import logo from "../assets/logo.png"
 function Navbar() {
   return (
     <div>
-        <nav className='flex '>
-            <div>
-                <img src={logo} alt="" />
-            </div>
-            <div>
-
-            <ul>
-                <li>Home</li>
-                <li>Services</li>
-                <li>About</li>
-                <li>Contact Us</li>
-            </ul>
-            </div>
-        </nav>
-      
+      <nav className="flex bg-black text-white ">
+        <div className='ml-5'>
+          <img className='w-28 h-28' src={logo} alt="" />
+        </div>
+        <div className='ml-auto my-auto mr-10'>
+          <ul className="flex gap-5 text-xl font-bold">
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Navbar
