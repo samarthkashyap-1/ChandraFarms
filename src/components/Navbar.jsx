6abc7bef936cc816c531922b197c4fd6bbc6d5fd
samarth kashyap <samarthkashyap318@gmail.com>
2023-09-03 23:19:52 +0530

    nav bar bug removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,7 +62,7 @@ function Navbar() {
         </div>
         <ul
           className={`flex gap-16 text-xl font-bold md:absolute md:w-[100%] md:right-0  md:py-10 md:top-0 md:bg-black md:bg-opacity-90 md:flex-col md:items-center box-border   ${
-            isOpen ? "md:top-0" : "md:-top-[150%]"
+            isOpen ? "md:top-0" : "md:-top-[100%]"
           } md:transition-all md:duration-1000 md:ease-in-out md:shadow}`}
         >
           <div className="md:block hidden">
